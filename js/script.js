ede0751be34b4e19b413782646d61e35bb33b4c3
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-05-31 14:28:52 +0200

    struttura

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//prompt e-mail
+var addressMails = ['perizat.shatyrbekova@gmail.com', 'hey@mail.ru', 'perlas@gmail.com', 'boolean@gmail.com', 'atom@yandex.ru' ];
+
+var mail = prompt ('Inserire la Sua e-mail');
+var check = false;
+
+for (var y = 0; y < addressMails.length; y++) {
+    if(addressMails [y] === mail) {
+        check = true;
+    }
+}
+
+if (check) {
+    document.getElementById('start').innerHTML += 'Lei è il nostro cliente!';
+} else {
+    document.getElementById('start').innerHTML += 'Si chiede di registrarsi con l\'e-mail!';
+}
